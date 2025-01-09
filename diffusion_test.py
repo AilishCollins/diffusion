7fd10b2e6466f7549c65bc694fabf589bbfd11be
Ailish Collins <ailishcollins@txstate.edu>
2025-01-09 17:07:10 +0000

    Add tests for initial profile and model grid

--- diffusion_test.py
+++ diffusion_test.py
@@ -1,10 +1,17 @@
 import math
-from Diffusion import calculate_time_step
+import numpy as np
+from diffusion import calculate_time_step, set_initial_profile, make_grid
+
 
 GRID_SPACING = 1.0
 DIFFUSIVITY = 1.0
 TIME_STEP = 0.5
 TOLERANCE = 0.01
+GRID_SIZE = 100
+BNDY_LEFT = 500
+BNDY_RIGHT = 0
+ORIGIN = 0
+DOMAIN_SIZE = GRID_SIZE * GRID_SPACING
 
 
 def test_time_step():
@@ -12,3 +19,18 @@ def test_time_step():
     assert type(time_step) is float
     assert time_step == 0.5
     assert math.isclose(time_step, TIME_STEP, rel_tol=TOLERANCE)
+
+
+def test_initial_profile():
+    profile = set_initial_profile()
+    assert type(profile) is np.ndarray
+    assert len(profile) == GRID_SIZE
+    assert profile[0] == BNDY_LEFT
+    assert math.isclose(profile[0], BNDY_LEFT)
+    assert math.isclose(profile[-1], BNDY_RIGHT)
+
+def test_make_grid():
+    grid, grid_size = make_grid(ORIGIN, ORIGIN+DOMAIN_SIZE, GRID_SPACING)
+    assert type(grid) is np.ndarray
+    assert type(grid_size) is int
+    
